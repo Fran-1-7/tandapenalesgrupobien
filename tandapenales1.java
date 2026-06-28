a7618a87b755609af5fdac506dd1d0d3d2dcf5bf
Fran <aku7sum4@gmail.com>
2026-06-28 18:34:59 -0300

    lo hice para 5 tandas de penalti el juego

--- tandapenales1.java
+++ tandapenales1.java
@@ -26,29 +26,66 @@ public class tandapenales1 {
 
         int posicionPateador;
         int posicionArquero;
+        int rondatotales = 5;
         boolean gol;
 
-        for (int jugador = 1; jugador <= 2; jugador++) {
+        //cotadores
+        int golesJugador1 = 0;
+        int golesJugador2 = 0;
 
-            System.out.println("=----------------------------------=");
-            System.out.println("Turno del Jugador " + jugador);
+        //bulce Rondas
+        for (int ronda = 1; ronda <= rondatotales; ronda++) {
+            System.out.println("====================================");
+            System.out.println("====================================");
+            System.out.println("--- Ronda numero " + ronda + " ---");
+            System.out.println("===================================="); 
+            //bucle para cada turno del jugador
+            for (int jugador = 1; jugador <= 2; jugador++){
+                System.out.println("====================================");
+                System.out.println("--- turno del jugador " + jugador + " ---");
 
-            mostrarOpciones();
+                mostrarOpciones();
 
-            System.out.print("Elija donde patear el penal: Opcion ");
-            posicionPateador = sc.nextInt();
+                System.out.print("Elija donde patear el penal: Opcion ");
+                posicionPateador = sc.nextInt();
+                //validamos opciones
+                    if (posicionPateador < 1 || posicionPateador > 3){
+                        System.out.print("Pateaste afuera");
+                    } else {
+                            posicionArquero = generarPosicionArquero();
 
-            posicionArquero = generarPosicionArquero();
+                            gol = validarPenal(posicionPateador, posicionArquero);
 
-            gol = validarPenal(posicionPateador, posicionArquero);
+                            System.out.println("Resultado del Jugador " + jugador);
+                            System.out.println("Posición del pateador: " + posicionPateador);
+                            System.out.println("Posición del arquero: " + posicionArquero);
+                            System.out.println("GOL: " + gol);
 
-            System.out.println("Resultado del Jugador " + jugador);
-            System.out.println("Posición del pateador: " + posicionPateador);
-            System.out.println("Posición del arquero: " + posicionArquero);
-            System.out.println("GOL: " + gol);
-        }
+                            //contador de goles para ambos jugadores
+                            if(gol){
+                                if (jugador == 1) {
+                                        golesJugador1 = golesJugador1 + 1;
+                                    } else if (jugador == 2){
+                                        golesJugador2 = golesJugador2 + 1;
+                                }
+                            }
+                        }
+                    }
+            }
 
-        System.out.println("=----------------------------------=");
+        System.out.println("====================================");
+        System.out.println("          RESULTADO FINAL           ");
+        System.out.println("====================================");
+        System.out.println("Goles Jugador 1: " + golesJugador1);
+        System.out.println("Goles Jugador 2: " + golesJugador2);
+        //Decidimos el Ganador o empate
+            if (golesJugador1 > golesJugador2) {
+            System.out.println("Ha Ganado el Jugador 1");
+            } else if (golesJugador2 > golesJugador1) {
+                System.out.println("Ha Ganado el Jugador 1");
+            } else {
+                System.out.println("Ha terminado en empate");
+            }
 
         sc.close();
     }
